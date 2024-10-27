699e667d1d0973bde2f6be7ae10ad58e9931b480
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-10-27 22:05:08 +0300

    12

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,4 +1,22 @@
 const getAllTasks = (req, res) => {
   res.send("all items");
 };
-module.exports = { getAllTasks };
+const getSingleTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+const createTask = (req, res) => {
+  res.json(req.body);
+};
+const patchTask = (req, res) => {
+  res.send("update item");
+};
+const deleteTask = (req, res) => {
+  res.send("delete item");
+};
+module.exports = {
+  getAllTasks,
+  getSingleTask,
+  createTask,
+  patchTask,
+  deleteTask,
+};
--- starter/routes/tasks.js
+++ starter/routes/tasks.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router(); //boilerplate
-const { getAllTasks } = require("../controllers/tasks");
+const {
+  getAllTasks,
+  getSingleTask,
+  createTask,
+  patchTask,
+  deleteTask,
+} = require("../controllers/tasks");
 
 router.get("/", getAllTasks); // we get the function from the controllers so we dont fill here up
+router.get("/:id", getSingleTask); // get single task with given ID
+router.post("/", createTask);
+router.patch("/:id", patchTask);
+router.delete("/:id", deleteTask);
 
 module.exports = router;
