f1208b2c4fb7759f4287ca675e5f5a854c6622bd
kaibutsutr <76039421+kaibutsutr@users.noreply.github.com>
2024-10-28 21:41:55 +0300

    fixed db

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose"); // we use mongoose to deal with db
 
 const connectDB = (url) => {
-  mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  }); // this is a promise so we have then and catch options. If successfull, then happens, if not error
+  mongoose.connect(url); // this is a promise so we have then and catch options. If successfull, then happens, if not error
 };
 module.exports = connectDB;
